b1da3834194ba8407783e927bf075731b8b500a2
zefs <54939609+zefs-cyber@users.noreply.github.com>
2022-12-20 13:12:06 +0600

    Update app.py

--- app.py
+++ app.py
@@ -176,4 +176,4 @@ def update_output(value):
     else:
         return part_c(value)
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server("0.0.0.0", debug = False, port=int(os.environ.get('PORT',8000)))
